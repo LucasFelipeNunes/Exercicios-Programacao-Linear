ad19050c948ce7c2181cad580815ef3d793cac65
Lucas Felipe Nunes <lucasfelipe.lfn@gmail.com>
2024-08-15 13:35:32 -0300

    Update Exercicio1.java

--- Exercicio1.java
+++ Exercicio1.java
@@ -5,12 +5,28 @@ import java.io.*;
 // 1. Dada uma matriz A qualquer, imprima a sua transposta A^T.
 class Main {
     public static void main(String[] args) {
-        int[][] A = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        for(int i = 0;i < A.length;i++){
-            System.out.println();
-            for(int j = 0;j < A[i].length;j++){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o número de linhas: ");
+        int linhas = scanner.nextInt();
+        System.out.print("Digite o número de colunas: ");
+        int colunas = scanner.nextInt();
+
+        int[][] A = new int[linhas][colunas];
+
+        System.out.println("Digite os elementos da matriz:");
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                A[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("A matriz transposta é:");
+        for (int i = 0; i < colunas; i++) {
+            for (int j = 0; j < linhas; j++) {
                 System.out.print(A[j][i] + " ");
             }
+            System.out.println();
         }
     }
 }
